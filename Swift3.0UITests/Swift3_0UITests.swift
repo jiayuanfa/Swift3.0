2bdaca307e9a478fc2d9a54f1da9b7aaaff5aa66
贾元发 <jiayuanfa@yeah.net>
2016-11-22 19:46:19 +0800

    Initial Commit

---
+++ Swift3.0UITests/Swift3_0UITests.swift
@@ -0,0 +1,36 @@
+//
+//  Swift3_0UITests.swift
+//  Swift3.0UITests
+//
+//  Created by Jafar的Apple Computer on 2016/11/22.
+//  Copyright © 2016年 com.quvideo. All rights reserved.
+//
+
+import XCTest
+
+class Swift3_0UITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
